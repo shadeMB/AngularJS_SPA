a0e0379af2728f2951a7edc527e954a4c45de4f5
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-09-29 12:57:31 +0300

    Testing dependency injection in JS/AngularJS

---
+++ module1-work/DependencyInjection/js/app.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('DIApp', []).controller('DIController', DIController);
+    
+    function DIController ($scope, $filter) {
+        $scope.name = "John";        
+    }
+
+})();
