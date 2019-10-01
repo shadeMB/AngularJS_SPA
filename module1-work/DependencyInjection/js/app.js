51953899cc34158145871309c41889748f72e7dd
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-10-01 21:49:47 +0300

    Testing dependency injection

--- module1-work/DependencyInjection/js/app.js
+++ module1-work/DependencyInjection/js/app.js
@@ -3,8 +3,14 @@
 
     angular.module('DIApp', []).controller('DIController', DIController);
     
-    function DIController ($scope, $filter) {
-        $scope.name = "John";        
-    }
+    DIController.$inject = ['$scope', '$filter'];
+    function DIController ($scope, $filter, $injector) {
+        $scope.name = "John";
+        $scope.upper = function () {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
 
+        console.log($injector.annotate(DIController));
+    }
 })();
