20bd307299f5af139c2605012318b1a160b7c732
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-10-01 22:56:59 +0300

    Added the hooks into the html

---
+++ module1-solution/js/app.js
@@ -0,0 +1,11 @@
+(function (){
+    'use strict';
+
+    angular.module("LunchCheck", [])
+    .controller("LunchCheckController", checkLunch);
+
+    checkLunch.$inject = ['$scope']
+    function checkLunch ($scope) {
+        
+    }
+})();
