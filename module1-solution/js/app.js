e0af56c75a88a091dc88f28983748b51e575a821
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-10-03 00:08:41 +0300

    Added the logic for the required functionality

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -6,8 +6,19 @@
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController ($scope) {
         $scope.message = "";
+        $scope.menu = "";
         $scope.checkLunch = function () {
-            console.log("hello");
+            var items = $scope.menu.split(",");
+            
+            if ($scope.menu.length === 0) {
+                $scope.message = "Please enter data first."
+            }
+            else if (items.length <= 3 && items.length > 0) {
+                $scope.message = "Enjoy!";
+            }
+            else {
+                $scope.message = "Too much!";
+            }
         };
     }
 })();
