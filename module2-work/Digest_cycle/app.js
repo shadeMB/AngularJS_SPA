be10d8eb4b907344a0946b3d00c4d4dc0583932d
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-10-06 23:02:30 +0300

    Digest cycle tests

--- module2-work/Digest_cycle/app.js
+++ module2-work/Digest_cycle/app.js
@@ -6,11 +6,30 @@ angular.module('CounterApp', [])
 
 CounterController.$inject = ['$scope'];
 function CounterController($scope) {
+  $scope.onceCounter = 0;
+  $scope.counter = 0;
 
   $scope.showNumberOfWatchers = function () {
-
+    console.log("# of watchers: " + $scope.$$watchersCount);
   };
 
+  $scope.countOnce = function () {
+    $scope.onceCounter = 1;
+  };
+
+  $scope.upCounter = function () {
+    $scope.counter++;
+  };
+
+  $scope.$watch('onceCounter', function (newValue, oldValue) {
+    console.log("Old value: ", oldValue);
+    console.log("New value: ", newValue);
+  });
+
+  $scope.$watch('counter', function (newValue, oldValue) {
+    console.log("counter old value: ", oldValue);
+    console.log("counter new value: ", newValue);
+  });
 }
 
 })();
