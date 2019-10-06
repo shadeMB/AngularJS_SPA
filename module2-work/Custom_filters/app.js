43baf6b7e413d23b4768b3c3936dba369cd7e927
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-10-06 19:38:10 +0300

    Testing custom filters

--- module2-work/Custom_filters/app.js
+++ module2-work/Custom_filters/app.js
@@ -2,23 +2,44 @@
 'use strict';
 
 angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+.controller('MsgController', MsgController)
+.filter('loves', LovesFilter)
+.filter('truth', TruthFilter);
 
-MsgController.$inject = ['$scope','$filter'];
-function MsgController($scope,$filter) {
-  $scope.name = "Yaakov";
+MsgController.$inject = ['$scope', 'lovesFilter'];
+function MsgController($scope, lovesFilter) {
   $scope.stateOfBeing = "hungry";
-  $scope.cookieCost = .45;
 
   $scope.sayMessage = function () {
     var msg = "Yaakov likes to eat healthy snacks at night!";
-    var output = $filter('uppercase')(msg);
-    return output;
+    return msg;
+  };
+
+  $scope.sayLovesMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    msg = lovesFilter(msg);
+    return msg;
   };
 
   $scope.feedYaakov = function () {
     $scope.stateOfBeing = "fed";
   };
+}
+
+function LovesFilter() {
+  return function (input) {
+    input = input || "";
+    input = input.replace("likes", "loves");
+    return input;
+  }
+}
+
+function TruthFilter() {
+  return function(input, target, replace) {
+    input = input || "";
+    input = input.replace(target, replace);
+    return input;
+  }
 }
 
 })();
